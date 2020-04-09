203ca34f8e555db69e29a2dd42d0ff8a119efcd4
Jacob Blomquist <jacobrb95@gmail.com>
2020-04-09 09:26:16 -0600

    Removed email from bots

--- tinyEndian.hpp
+++ tinyEndian.hpp
@@ -1,5 +1,5 @@
 /*
-		Copyright 2020 Jacob R. Blomquist (Jacobrb95@gmail.com)
+		Copyright 2020 Jacob R. Blomquist (Jacobrb95 AT gmail DOT com)
 
 		Permission is hereby granted, free of charge, to any person obtaining a copy of
 		this software and associated documentation files (the "Software"), to deal in
